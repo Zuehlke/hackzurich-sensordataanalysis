827101eceaa9b9dc90b927a6007ced4ed0b45624
Michael Springmann <michael.springmann@zuehlke.com>
2016-08-26 17:43:46 +0200

    Don't create topic from device id. Just use a single (configurable) topic per service, default is "sensor-reading" and use the device id as key.
    This would still guarantee  that all messages from the same device end up in the same partition (thus even guaranteeing ordering) while different devices can be balanced on different partitions and therefore also on different nodes, making it ease to scale with the number of sensing devices.
     For proper analysis of the data, it is now of course essential to only correlate data of the same key... except for Chasing Mr. X, for that you need to correlate based on time with potentially some drift.

--- sensor-ingestion/src/main/scala/com/zuehlke/hackzurich/configuration/RestIngestionConfiguration.scala
+++ sensor-ingestion/src/main/scala/com/zuehlke/hackzurich/configuration/RestIngestionConfiguration.scala
@@ -5,4 +5,5 @@ import org.apache.commons.lang3.StringUtils
 object RestIngestionConfiguration {
   val HOSTNAME: String = StringUtils.defaultString(System.getenv("HOSTNAME"), "localhost")
   val PORT: Int = Integer.valueOf(StringUtils.defaultString(System.getenv("PORT0"), "18080"))
+  val TOPIC: String = StringUtils.defaultString(System.getenv("KAFKA_TOPIC"), "sensor-reading")
 }
