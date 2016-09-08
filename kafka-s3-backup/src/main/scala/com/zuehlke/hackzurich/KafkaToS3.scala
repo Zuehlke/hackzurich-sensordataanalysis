94c56b4ebff4a16de1042607c6b2feaee703f805
Michael Springmann <michael.springmann@zuehlke.com>
2016-09-08 14:17:51 +0200

    KafkaToS3 adapted to Kafka 10.
    Also docuemnted how to perform a full backup, not just a backup of everything that is submitted now.

--- kafka-s3-backup/src/main/scala/com/zuehlke/hackzurich/KafkaToS3.scala
+++ kafka-s3-backup/src/main/scala/com/zuehlke/hackzurich/KafkaToS3.scala
@@ -3,16 +3,17 @@ package com.zuehlke.hackzurich
 import java.util.Calendar
 
 import com.zuehlke.hackzurich.common.kafkautils.MesosKafkaBootstrapper
-import kafka.serializer.StringDecoder
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming._
-import org.apache.spark.streaming.kafka._
+import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils, LocationStrategies}
 
 /**
   * Consumes messages from one or more topics in Kafka and puts them into an S3 bucket.
   *
   * Run in dcos with:
-  * dcos spark run --submit-args="--supervise --conf spark.mesos.uris=http://hdfs.marathon.mesos:9000/v1/connect/hdfs-site.xml,http://hdfs.marathon.mesos:9000/v1/connect/core-site.xml --class com.zuehlke.hackzurich.KafkaToS3 <jar_location> <topics> <awsId> <awsSecret>
+  * dcos spark run --submit-args="--supervise --conf spark.mesos.uris=http://hdfs.marathon.mesos:9000/v1/connect/hdfs-site.xml,http://hdfs.marathon.mesos:9000/v1/connect/core-site.xml --class com.zuehlke.hackzurich.KafkaToS3 <jar_location> <topics> <s3bucket> <awsId> <awsSecret>"
   */
 object KafkaToS3 {
   def main(args: Array[String]) {
@@ -20,7 +21,7 @@ object KafkaToS3 {
       System.err.println(s"""
                             |Usage: KafkaToS3 <topics> <s3bucket> <awsAccessKeyId> <awsAccesKeySecret>
                             |  <topics> is a list of one or more kafka topics to consume from
-                            |  <s3bucket> the s3bucket to store the files into
+                            |  <s3bucket> the s3bucket to store the files into (s3bucket may contain subfolders, will get created if needed)
                             |  <awsAccessKeyId> the aws access key
                             |  <awsAccesKeySecret> the aws access secret
         """.stripMargin)
@@ -29,26 +30,42 @@ object KafkaToS3 {
 
     val Array(topics, s3bucket, awsId, awsSecret) = args
 
-    // Create context with 2 second batch interval
+    // Create context with 30 second batch interval
     val sparkConf = new SparkConf().setAppName("KafkaToS3")
     val ssc = new StreamingContext(sparkConf, Seconds(30))
 
+    // use Hadoop HDFS tooling to access S3
     ssc.sparkContext.hadoopConfiguration.set("fs.s3n.awsAccessKeyId",awsId)
     ssc.sparkContext.hadoopConfiguration.set("fs.s3n.awsSecretAccessKey",awsSecret)
 
     // Create direct kafka stream with brokers and topics
+    val kafkaBrokers = MesosKafkaBootstrapper.mkBootstrapServersString
+    println(s"Reading stream for topics ${topics} using brokers ${kafkaBrokers}")
     val topicsSet = topics.split(",").toSet
-    val kafkaParams = Map[String, String]("metadata.broker.list" -> MesosKafkaBootstrapper.mkBootstrapServersString)
-    val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
-      ssc, kafkaParams, topicsSet)
+
+    // although we assume topics to be generic, we need to assume or make configurable the types of keys/values and corresponding deserializers
+    val defaultDeserializer = "org.apache.kafka.common.serialization.StringDeserializer"
+
+    val groupID = "KafkaToS3" + topics + s3bucket;
+
+    val kafkaParams = Map[String, String](
+      ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> kafkaBrokers,
+      // ConsumerConfig.AUTO_OFFSET_RESET_CONFIG -> "earliest", // reads the oldest entries available in Kafka. Other possibility is to receive data from now on ("latest"). Default is to continue wherever a consumer with same group ID was or if it is a new group ID, start with latest.
+      ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG -> defaultDeserializer,
+      ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG -> defaultDeserializer,
+      ConsumerConfig.GROUP_ID_CONFIG -> groupID
+    )
+
+    val messages = KafkaUtils.createDirectStream[String, String](
+      ssc, LocationStrategies.PreferConsistent, ConsumerStrategies.Subscribe[String, String](topicsSet, kafkaParams))
 
     // Save to S3
     messages.foreachRDD(rdd => {
-
       println("\n\nNumber of records in this batch : " + rdd.count())
-      if(rdd.count() > 0){
-        rdd.repartition(1).saveAsTextFile("s3n://" + s3bucket + "/" + Calendar.getInstance().getTime())
-      }
+        if(rdd.count() > 0){
+          rdd.flatMap(record => s"${record.key} : ${record.value()}")
+          .repartition(1).saveAsTextFile("s3n://" + s3bucket + "/" + Calendar.getInstance().getTime())
+        }
     })
 
     // Start the computation
