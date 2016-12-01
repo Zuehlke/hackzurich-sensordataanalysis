93ddf9d9edd27f10f7c5c8b7cc01129765da374a
Benjamin Grenacher <bgrenacher@gmail.com>
2016-12-01 14:48:41 +0100

    LightReading with json4s

---
+++ common-utils/src/main/scala/com/zuehlke/hackzurich/common/dataformats/LightReadingJSON4S.scala
@@ -0,0 +1,24 @@
+package com.zuehlke.hackzurich.common.dataformats
+
+import org.apache.log4j.LogManager
+
+import scala.util.control.NonFatal
+
+// Hint: Use only lowercase characters in case classes to avoid trouble when storing data in Cassandra,
+// where rules for column names my be different
+
+/** See https://github.com/Zuehlke/hackzurich-sensordata-ios/blob/master/README.md#light */
+case class LightReadingJSON4S(deviceid : String, date : String, brightnes: Double, sensortype: String = "Light")
+object LightReadingJSON4S {
+  import org.json4s._
+  def from(t: (String, JValue)): Option[LightReadingJSON4S] = {
+    implicit val formats = DefaultFormats
+    try Some(LightReadingJSON4S(
+      t._1,
+      (t._2 \ "date").extract[String],
+      (t._2 \ "brightnes").extract[Double]))
+    catch {
+      case NonFatal(e) => LogManager.getLogger(LightReading.getClass).warn("Failed to get data from json. Possible wrong format: " + e); None
+    }
+  }
+}
