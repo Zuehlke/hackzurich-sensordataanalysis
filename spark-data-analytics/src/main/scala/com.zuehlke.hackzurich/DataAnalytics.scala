399f089f8f1e3f3cd6f51fc426a15db8e3f8c40f
Benedikt Wedenik <benedikt.wedenik@gmail.com>
2018-02-01 16:47:22 +0100

    Added more documentation and polling data from Cassandra in a loop

--- spark-data-analytics/src/main/scala/com.zuehlke.hackzurich/DataAnalytics.scala
+++ spark-data-analytics/src/main/scala/com.zuehlke.hackzurich/DataAnalytics.scala
@@ -11,9 +11,9 @@ import com.zuehlke.hackzurich.common.kafkautils.MesosKafkaBootstrapper
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 import org.apache.spark.SparkConf
 import org.apache.spark.mllib.linalg.DenseVector
-import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.expressions.UserDefinedFunction
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 
 /**
@@ -30,6 +30,12 @@ object DataAnalytics {
     * Initial value of latest timestamp
     */
   var latestDate = 0l
+
+  /**
+    * The global DataFrame which is holding the observations aggregated from the Cassandra DB
+    * This variable is updated after polling by unioning the new data
+    */
+  var observationsDf: DataFrame = null
 
   /**
     * How far in the future we want to predict
@@ -71,97 +77,102 @@ object DataAnalytics {
     producerProps.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
     producerProps.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
 
-    // Read data from Cassandra
-    val barometerDf = spark
-      .read
-      .format("org.apache.spark.sql.cassandra")
-      .options(Map(
-        "keyspace" -> "sensordata",
-        "table" -> "barometer"))
-      .load()
-
-    // Aggregate data to a precision of seconds and calculate mean
-    var observations = barometerDf.select("date", "deviceid", "relativealtitude")
-    observations = observations.groupBy(
-      year(col("date")).alias("year"),
-      month(col("date")).alias("month"),
-      dayofmonth(col("date")).alias("day"),
-      hour(col("date")).alias("hour"),
-      minute(col("date")).alias("minute"),
-      second(col("date")).alias("second"),
-      col("deviceid")
-    ).agg(
-      count(col("deviceid")).as("number_of_devices"),
-      mean("relativealtitude").as("relativealtitude_average"))
-
-    // Add converted (aggregated) date back again
-    observations = observations.withColumn("timestamp", toDateUdf.apply(col("year"), col("month"), col("day"), col("hour"), col("minute"), col("second")))
-    observations.show(10, false)
-
-    //////////////////////////////////////////////////////////////////////////
-    //////////////////////////// PREDICTION PART /////////////////////////////
-    //////////////////////////////////////////////////////////////////////////
-
-    val firstDate: Timestamp = observations.select("timestamp").orderBy(asc("timestamp")).head().getAs[Timestamp]("timestamp")
-    val lastDate: Timestamp = observations.select("timestamp").orderBy(desc("timestamp")).head().getAs[Timestamp]("timestamp")
-
-    // Create a DateTimeIndex over the whole range of input data in seconds interval
-    val zone = ZoneId.systemDefault()
-    val dtIndex = DateTimeIndex.uniformFromInterval(
-      ZonedDateTime.of(firstDate.toLocalDateTime, zone),
-      ZonedDateTime.of(lastDate.toLocalDateTime, zone),
-      new SecondFrequency(1))
-
-    // Align the data on the DateTimeIndex to create a TimeSeriesRDD
-    val timeSeriesrdd = TimeSeriesRDD.timeSeriesRDDFromObservations(dtIndex, observations,
-      "timestamp", "deviceid", "relativealtitude_average")
-
-    // Compute missing values using linear interpolation
-    val interpolatedTimeSeriesrdd = timeSeriesrdd.fill("linear")
-
-    val forecast = interpolatedTimeSeriesrdd.mapSeries { vector =>
-      /*
-      * If we are missing some data in the beginning or at the end (so everything we could not interpolate)
-      * we just give the prediction based on the latest data we have (which is everything != NaN)
-      */
-      val newVec = new DenseVector(vector.toArray.filter(!_.isNaN))
-
-      val arimaModel = ARIMA.fitModel(1, 1, 0, newVec)
-      val forecasted = arimaModel.forecast(newVec, forecastTime)
-      new DenseVector(forecasted.toArray.slice(forecasted.size - forecastTime, forecasted.size))
-    }
-
-    forecast.foreach { x =>
-      val timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(System.currentTimeMillis())
-      val message = s"$timestamp forecast size: ${x._2.size} , $x , ${x._2}"
-      new KafkaProducer[String, String](producerProps).send(new ProducerRecord("debug", System.currentTimeMillis().toString, message))
-    }
-
-    //////////////////////////////////////////////////////////////////////////
-    //////////////////////////// PREDICTION PART /////////////////////////////
-    //////////////////////////////////////////////////////////////////////////
-
+    /*
+     * We do this whole stuff in a loop because we want to poll Cassandra regularly
+     */
     while (true) {
-      barometerDf
-        .where(s"date > '$latestDate'")
-        .foreach { x =>
-          val producer = new KafkaProducer[String, String](producerProps)
-          val date = x.getAs[Timestamp]("date")
-          val dateFormatted = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(date)
-          val deviceid = x.getAs[String]("deviceid")
-          val relativealtitude = x.getAs[Double]("relativealtitude")
-          val pressure = x.getAs[Double]("pressure")
-          val message = s"$dateFormatted - $deviceid - $relativealtitude - $pressure"
-          println(message)
 
-          val record = new ProducerRecord("data-analytics", "barometer", message)
-          producer.send(record)
-          updateLatestDate(date.getTime)
-        }
+      // Read data from Cassandra
+      var barometerDf = spark
+        .read
+        .format("org.apache.spark.sql.cassandra")
+        .options(Map(
+          "keyspace" -> "sensordata",
+          "table" -> "barometer"))
+        .load()
 
-      Thread.sleep(15 * 1000)
+      // If we've never loaded any data from Cassandra we don't want to filter out anything
+      if (observationsDf != null) {
+        // This filter is pushed down to Cassandra and only serialized the data we need!
+        barometerDf = barometerDf.filter(s"date > '$latestDate'")
+      }
+
+      // Aggregate data to a precision of seconds and calculate the mean
+      var observations = barometerDf.select("date", "deviceid", "relativealtitude")
+      observations = observations.groupBy(
+        year(col("date")).alias("year"),
+        month(col("date")).alias("month"),
+        dayofmonth(col("date")).alias("day"),
+        hour(col("date")).alias("hour"),
+        minute(col("date")).alias("minute"),
+        second(col("date")).alias("second"),
+        col("deviceid")
+      ).agg(
+        count(col("deviceid")).as("number_of_devices"),
+        mean("relativealtitude").as("relativealtitude_average"))
+
+      // Add converted (aggregated) date back again
+      observations = observations.withColumn("timestamp", toDateUdf.apply(col("year"), col("month"), col("day"), col("hour"), col("minute"), col("second")))
+
+      if (observationsDf == null) {
+        observationsDf = observations
+      } else {
+        observationsDf = observationsDf.union(observations).distinct()
+      }
+
+      println(s"observationsDf.count() = ${observationsDf.count()}")
+      observationsDf.show(10, false)
+
+      //////////////////////////////////////////////////////////////////////////
+      //////////////////////////// PREDICTION PART /////////////////////////////
+      //////////////////////////////////////////////////////////////////////////
+
+      val firstDate: Timestamp = observationsDf.select("timestamp").orderBy(asc("timestamp")).head().getAs[Timestamp]("timestamp")
+      val lastDate: Timestamp = observationsDf.select("timestamp").orderBy(desc("timestamp")).head().getAs[Timestamp]("timestamp")
+
+      println(s"firstDate: $firstDate , lastDate: $lastDate")
+
+      // Create a DateTimeIndex over the whole range of input data in seconds interval
+      val zone = ZoneId.systemDefault()
+      val dtIndex = DateTimeIndex.uniformFromInterval(
+        ZonedDateTime.of(firstDate.toLocalDateTime, zone),
+        ZonedDateTime.of(lastDate.toLocalDateTime, zone),
+        new SecondFrequency(1))
+
+      // Align the data on the DateTimeIndex to create a TimeSeriesRDD
+      val timeSeriesrdd = TimeSeriesRDD.timeSeriesRDDFromObservations(dtIndex, observationsDf,
+        "timestamp", "deviceid", "relativealtitude_average")
+
+      // Compute missing values using linear interpolation
+      val interpolatedTimeSeriesrdd = timeSeriesrdd.fill("linear")
+
+      val forecast = interpolatedTimeSeriesrdd.mapSeries { vector =>
+        /*
+        * If we are missing some data in the beginning or at the end (so everything we could not interpolate)
+        * we just give the prediction based on the latest data we have (which is everything != NaN)
+        */
+        val newVec = new DenseVector(vector.toArray.filter(!_.isNaN))
+        require(newVec.size > 0)
+
+        val arimaModel = ARIMA.fitModel(1, 1, 0, newVec)
+        val forecasted = arimaModel.forecast(newVec, forecastTime)
+        new DenseVector(forecasted.toArray.slice(forecasted.size - forecastTime, forecasted.size))
+      }
+
+      forecast.foreach { x =>
+        val timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(System.currentTimeMillis())
+        val message = s"$timestamp forecast size: ${x._2.size} , $x}"
+        new KafkaProducer[String, String](producerProps).send(new ProducerRecord("data-analytics", x._1, message))
+      }
+
+      //////////////////////////////////////////////////////////////////////////
+      //////////////////////////// PREDICTION PART /////////////////////////////
+      //////////////////////////////////////////////////////////////////////////
+
+      updateLatestDate(lastDate.getTime)
     }
 
     sc.stop()
   }
+
 }
